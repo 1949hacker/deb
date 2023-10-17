0b466cee251f18aed94293248984738e8989f8a0
Scepter <scepter@1949hacker.cn>
2023-10-17 15:09:20 +0800

    test

---
+++ python-source/aptdownloader.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+简介：
+    调用apt-rdepends包查询目标软件包所需依赖，再使用apt download命令下载到当前目录
+语法：
+    aptdownloader <包名>
+作者：
+    Vladimir Yang
+    scepter@1949hacker.cn
+    https://1949hacker.cn
+"""
+
+import subprocess
+
+if __name__ == "__main__":
+    pgName = input("")
+    cmd = ["apt", "download", "$(apt-rdepends", "-p", pgName, "|", "grep", \
+           "-v",
+           "\"^\0", "\")"]
+    aptInstall = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
+    output = aptInstall.communicate()[0].decode("utf-8")
+    print(output)
